145f7f80a105fc62ef4cea0b92545903da4c0574
ganeodolu <ganeodolu@gmail.com>
2020-03-25 21:38:38 +0900

    fix: 초기화

---
+++ backend/routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const DustService = require('../api/modules/dust');
+const router = express.Router();
+
+router.get('/me/pm10', async (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  const nearStations = await DustService.getNearByMsrstnsUri(
+    req.query.lat,
+    req.query.lng,
+  );
+  const dustStatus = await DustService.getStatusByStationName(
+    nearStations.data.list[0].stationName,
+  );
+  res.status(200).json({
+    pm10: dustStatus.data.list[0].pm10Grade,
+  });
+});
+
+module.exports = router;
---
+++ webpack.config.js
@@ -0,0 +1,126 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+// const childProcess = require('child_process');
+// Is build option is development
+const isProduction = process.env.NODE_ENV === 'production';
+
+const entryPath = path.join(__dirname, './src/app.js');
+const outputPath = path.resolve(__dirname, 'dist');
+const title = 'Vanilla BoilerPlate';
+const mode = process.env.NODE_ENV || 'development';
+
+const banner = `
+*     Author: Black Coffe
+*     Description: Vanilla BoilerPlate
+*     Source: https://github.com/amorfati0310/vanilla-boilerplate
+*     BuildDate: ${new Date().toLocaleString()}
+*     Hash: [hash]
+*     Chunkhash: [chunkhash]
+*     File: [file]
+
+`;
+
+// netify git config error
+//*     Contributor: ${childProcess.execSync('git config user.name')}
+//*     Commit: ${childProcess.execSync('git rev-parse --short HEAD  ')}
+
+module.exports = {
+  mode,
+  entry: {
+    app: entryPath,
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: outputPath,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: isProduction
+              ? MiniCssExtractPlugin.loader // 프로덕션 환경
+              : 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {modules: true},
+          },
+          {loader: 'sass-loader'},
+        ],
+      },
+      {
+        test: /\.m?(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        loader: 'url-loader',
+        options: {
+          name: '[path][name].[ext]?[hash]',
+          limit: 8192,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner,
+    }),
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Custom template',
+      template: path.join(__dirname, 'src/index.html'),
+      minify: isProduction
+        ? {
+            collapseWhitespace: true, // 빈칸 제거
+            removeComments: true, // 주석 제거
+          }
+        : false,
+      hash: isProduction,
+      templateParameters: {
+        env: isProduction ? '' : 'Dev',
+        title,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    ...(isProduction
+      ? [new MiniCssExtractPlugin({filename: '[name].css'})]
+      : []),
+  ],
+  stats: {
+    colors: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'src'),
+    },
+    extensions: ['*', '.js', '.json', '.jsx'],
+  },
+  optimization: {
+    minimizer: isProduction
+      ? [
+          new TerserPlugin({
+            terserOptions: {
+              compress: {
+                drop_console: true,
+              },
+            },
+          }),
+        ]
+      : [],
+  },
+};
